ca9829087735d77aa3cf507c7b891b258afda6bf
Ian Cobb <iancobb@Ians-MacBook-Pro.local>
2017-09-28 09:40:21 -0700

    Created sms stub for tests

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -6,7 +6,6 @@ Bundler.require(*Rails.groups)
 
 module Overtime
   class Application < Rails::Application
-    config.load_defaults 5.1
     config.autoload_paths << Rails.root.join("lib")
   end
 end
